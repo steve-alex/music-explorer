bc9610e63fd7e113868827a0d75e4d791a846820
Steve Alex <steve.alex@hotmail.co.uk>
2020-12-11 16:26:20 +0000

    Create app structure

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import MainController from './Controllers/MainController';
+import AuthController from './Controllers/AuthController';
+
+class App {
+  public port: number;
+  public app: express.Application;
+
+  constructor(port: number){
+    this.app = express();
+    this.port = port;
+
+    this.connectToDatabase()
+    this.initializeMiddlewares();
+    this.initiatilzeControllers();
+  }
+
+  public listen(){
+    this.app.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    })
+  }
+
+  private connectToDatabase(){
+    // TODO - Connect to the database
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(express.static(__dirname + '/public'))
+    this.app.use(express.json());
+    this.app.use(cors())
+    this.app.use(cookieParser());
+  }
+
+  private initiatilzeControllers(){
+    this.app.use(new MainController().router);
+    this.app.use(new AuthController().router);
+  }
+}
+
+export default App;
